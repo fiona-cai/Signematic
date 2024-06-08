5d8793ba7063a809032c5712505ace74a79ba924
Soheil Rajabali <soheilrajabali@gmail.com>
2024-06-08 03:29:41 -0400

    database created

--- HandNodes.py
+++ HandNodes.py
@@ -1,80 +1,44 @@
 import cv2
 import mediapipe as mp
+import os
+import json
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = []
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+file = open("hand_landmarks.json", "w")
 
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-    # Draw hand world landmarks.
-    if not results.multi_hand_world_landmarks:
-      continue
-    for hand_world_landmarks in results.multi_hand_world_landmarks:
-      mp_drawing.plot_landmarks(
-        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
-
-# For webcam input:
-cap = cv2.VideoCapture("test.mp4")
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      break
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5) as hands:
+  data={}
+  for idx, videoFile in enumerate(os.listdir('videos/')):
+    if videoFile.endswith('.mp4'):
+        video = os.path.join('videos/', videoFile)
+        data[video.split(".")[0].split("/")[-1]] = []
+        cap = cv2.VideoCapture(video)
+        frame = 0
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                break
+            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            if results.multi_hand_landmarks:
+                handCords = []
+                for hand_landmarks in results.multi_hand_landmarks:
+                    hand=hand_landmarks.landmark
+                    if hand[mp_hands.HandLandmark.WRIST].x < hand[mp_hands.HandLandmark.THUMB_TIP].x:
+                        handType = "Left"
+                    else:
+                        handType = "Right"
+                    for joint_id, landmark in enumerate(hand):
+                        x,y,z=landmark.x,landmark.y,landmark.z
+                        handCords.append({"joint_id":joint_id,"x":x,"y":y,"z":z})
+                data[video.split(".")[0].split("/")[-1]].append({
+                    "Frame Number: ": frame,
+                    "Left Hand Coordinates: ": handCords if handType == "Left" else[],
+                    "Right Hand Coordinates: ": handCords if handType == "Right" else[]
+                })
+                frame += 1
+        cap.release()
+    json.dump(data, file)
+file.close()
