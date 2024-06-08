7ace447d2d42f4eec05f06b5f715c9b29ffcc197
Fiona Cai <fiona.cai899@gmail.com>
2024-06-08 14:03:54 -0400

    pushhy

--- autosubtitle.py
+++ autosubtitle.py
@@ -4,26 +4,40 @@ import yt_dlp as youtube_dl
 from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 
-YDL_OPTIONS = {'format': 'bestvideo+bestaudio', 'noplaylist': 'True'}
+YDL_OPTIONS_VIDEO = {
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', 
+    'noplaylist': 'True',
+}
 
-def download_video(song, time_length):
-    results = YoutubeSearch(song, max_results=10).to_dict()
+YDL_OPTIONS_AUDIO = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+    'noplaylist': 'True',
+}
+
+def download_video_and_audio(song, time_length):
+    results = YoutubeSearch(song, max_results=15).to_dict()
 
     for data in results:
         if len(data['duration'].split(":")) < 3:
             time = (int(data["duration"].split(":")[0]) * 60) + int(data["duration"].split(":")[1])
             if abs(int(time) - int(time_length)) < 3:
                 title = data["title"]
-                with YoutubeDL(YDL_OPTIONS) as ydl:
+                with YoutubeDL(YDL_OPTIONS_VIDEO) as ydl_video, YoutubeDL(YDL_OPTIONS_AUDIO) as ydl_audio:
                     try:
                         get(title)
                     except:
-                        video = ydl.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
+                        video_info = ydl_video.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
+                        audio_info = ydl_audio.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
                     else:
-                        video = ydl.extract_info(title, download=True)
-                filename = os.path.splitext(ydl.prepare_filename(video))[:-1]
+                        video_info = ydl_video.extract_info(title, download=True)
+                        audio_info = ydl_audio.extract_info(title, download=True)
+                filename_video = os.path.splitext(ydl_video.prepare_filename(video_info))[:-1]
+                filename_audio = os.path.splitext(ydl_audio.prepare_filename(audio_info))[:-1]
                 break
-    return ''.join(filename) + ".mp4"
+    return ''.join(filename_video) + ".mp4", ''.join(filename_audio) + ".wav"
 
-
-video_file = download_video("Comparing WATERLOO Housing Prices | Village1/UWP", 378)
+video_file, audio_file = download_video_and_audio("The Wait", 60)
---
+++ video-to-text.py
@@ -0,0 +1,50 @@
+from requests import get
+import os
+import yt_dlp as youtube_dl
+from yt_dlp import YoutubeDL
+from youtube_search import YoutubeSearch
+
+YDL_OPTIONS_VIDEO = {
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', 
+    'noplaylist': 'True',
+}
+
+YDL_OPTIONS_AUDIO = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+    'noplaylist': 'True',
+}
+
+filename=""
+
+def download_video_and_audio(song, time_length):
+    results = YoutubeSearch(song, max_results=15).to_dict()
+
+    for data in results:
+        if len(data['duration'].split(":")) < 3:
+            time = (int(data["duration"].split(":")[0]) * 60) + int(data["duration"].split(":")[1])
+            if abs(int(time) - int(time_length)) < 3:
+                title = data["title"]
+                with YoutubeDL(YDL_OPTIONS_VIDEO) as ydl_video, YoutubeDL(YDL_OPTIONS_AUDIO) as ydl_audio:
+                    try:
+                        get(title)
+                    except:
+                        video_info = ydl_video.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
+                        audio_info = ydl_audio.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
+                    else:
+                        video_info = ydl_video.extract_info(title, download=True)
+                        audio_info = ydl_audio.extract_info(title, download=True)
+                filename_video = os.path.splitext(ydl_video.prepare_filename(video_info))[:-1]
+                filename_audio = os.path.splitext(ydl_audio.prepare_filename(audio_info))[:-1]
+                break
+    return ''.join(filename_video) + ".mp4", ''.join(filename_audio) + ".wav"
+
+text = speech_to_text(uploaded_file)
+    print("Converted Text:")
+    print(text)
+
+video_file, audio_file = download_video_and_audio("The Wait", 60)
+
