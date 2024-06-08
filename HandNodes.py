5c9108010ea9043f66a0006ad5bcd26d886c0dcd
Soheil Rajabali <soheilrajabali@gmail.com>
2024-06-08 11:57:06 -0400

    skeleton hand go ooga booga

--- HandNodes.py
+++ HandNodes.py
@@ -24,7 +24,7 @@ with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confi
             image=cv2.resize(image,(640,480))
             results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             if results.multi_hand_landmarks:
-                handCords = []
+                handCords = {}
                 for hand_landmarks in results.multi_hand_landmarks:
                     hand=hand_landmarks.landmark
                     if hand[mp_hands.HandLandmark.WRIST].x < hand[mp_hands.HandLandmark.THUMB_TIP].x:
@@ -33,21 +33,20 @@ with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confi
                         handType = "Right"
                     for joint_id, landmark in enumerate(hand):
                         x,y,z=landmark.x,landmark.y,landmark.z
-                        handCords.append({"joint_id":joint_id,"x":x,"y":y,"z":z})
+                        handCords[joint_id]=[x,y,z]
                     mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 data[video.split(".")[0].split("/")[-1]].append({
-                    "Frame Number: ": frame,
-                    "Left Hand Coordinates: ": handCords if handType == "Left" else[],
-                    "Right Hand Coordinates: ": handCords if handType == "Right" else[]
+                    "Frame Number": frame,
+                    "Left Hand Coordinates": handCords if handType == "Left" else[],
+                    "Right Hand Coordinates": handCords if handType == "Right" else[]
                 })
                 frame += 1
                 cv2.imshow('Hand Tracking', image)
                 if cv2.waitKey(5) & 0xFF == ord('q'):
                     break
         cap.release()
-        data[video.split(".")[0].split("/")[-1]].append({
-            "Total Frames: ": frame
-        })
+        data[video.split(".")[0].split("/")[-1]].append({"Total Frames": frame})
+        print(data[video.split(".")[0].split("/")[-1]][-1]["Total Frames"])
         print("Hand Landmarks for video: ", video.split("/")[-1], " extracted successfully with ", str(frame), " frames")
     json.dump(data, file)
 file.close()
--- main.js
+++ main.js
@@ -1,26 +1,115 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+
+var container = document.getElementById("ASL-Gestures");
+const fov = 75;
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1;
+const far = 1000;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var wordList = ["internet"]
+var wordIndex = 0;
+var frameIndex = 0;
 
-camera.position.z = 5;
+fetch('hand_landmarks.json')
+  .then(response => response.json())
+  .then(data => {
+    function drawPoint(x, y, z) {
+      const pointRadius = 0.25;
+      const geometry = new THREE.SphereGeometry(pointRadius, 32, 16);
+      const material = new THREE.MeshBasicMaterial({ color: 0x84FFFF });
+      const sphere = new THREE.Mesh(geometry, material); scene.add(sphere);
+      sphere.position.x = x;
+      sphere.position.y = y;
+      sphere.position.z = z;
+    }
 
-function animate() {
-  requestAnimationFrame(animate);
+    function drawLine(x1, y1, z1, x2, y2, z2) {
+      const points = [];
+      points.push(new THREE.Vector3(x1, y1, z1));
+      points.push(new THREE.Vector3(x2, y2, z2));
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      const material = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
+      const line = new THREE.Line(geometry, material);
+      scene.add(line);
+    }
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    function redistributeElements(left, right) {
+      if (left.length > 21) {
+        const redistributedElements = left.splice(21);
+        right.push(...redistributedElements);
+      } else if (right.length > 21) {
+        const redistributedElements = right.splice(21);
+        left.push(...redistributedElements);
+      }
+    }
 
-  renderer.render(scene, camera);
-}
+    function connectLines(frameIndex) {
+      const nodeConnections = [[0, 1], [1, 2], [2, 3], [3, 4], [0, 5], [5, 6], [6, 7], [7, 8], [5, 9], [9, 10], [10, 11], [11, 12], [9, 13], [13, 14], [14, 15], [15, 16], [13, 17], [17, 18], [18, 19], [19, 20], [0, 17]];
+      var left = data[wordList[wordIndex]][frameIndex]['Left Hand Coordinates'];
+      var right = data[wordList[wordIndex]][frameIndex]['Right Hand Coordinates'];
 
-animate();
+      redistributeElements(left, right);
+
+      nodeConnections.forEach(function (node) {
+        const u = node[0];
+        const v = node[1];
+        if (left[u] && left[v]) {
+          const l1 = left[u];
+          const l2 = left[v];
+          drawLine(l1[0] * 50, l1[1] * -50, l1[2] * 50, l2[0] * 50, l2[1] * -50, l2[2] * 50);
+        }
+        if (right[u] && right[v]) {
+          const r1 = right[u];
+          const r2 = right[v];
+          drawLine(r1[0] * 50, r1[1] * -50, r1[2] * 50, r2[0] * 50, r2[1] * -50, r2[2] * 50);
+        }
+      })
+    }
+
+    let clock = new THREE.Clock();
+    let delta = 0;
+    let interval = 1 / 45;
+
+    function render() {
+      requestAnimationFrame(render);
+      delta += clock.getDelta();
+
+      if (delta > interval) {
+        delta = delta % interval;
+
+        if (wordList.length > 0 && wordIndex < wordList.length) {
+
+          var left = data[wordList[wordIndex]][frameIndex]['Left Hand Coordinates'];
+          var right = data[wordList[wordIndex]][frameIndex]['Right Hand Coordinates'];
+
+          for(var i = 0; i < left.length; i++) {
+            drawPoint(left[i][0] * 50, left[i][1] * -50, left[i][2] * 50);
+          }
+          for(var i = 0; i < right.length; i++) {
+            drawPoint(right[i][0] * 50, right[i][1] * -50, right[i][2] * 50);
+          }
+          connectLines(frameIndex);
+
+          frameIndex++;
+          if (frameIndex >= data[wordList[wordIndex]].length) {
+            frameIndex = 0;
+            wordIndex++;
+          }
+        }
+        renderer.render(scene, camera);
+        scene.remove.apply(scene, scene.children);
+      }
+    }
+
+    render();
+  })
+
+camera.position.set(27.5, -30, 25);
