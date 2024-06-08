cde879691c2a56c12f51b35c60874157cb8403c0
Fiona Cai <fiona.cai899@gmail.com>
2024-06-08 16:32:44 -0400

    subtitles file write to file

--- video-to-text.py
+++ video-to-text.py
@@ -116,3 +116,7 @@ video_file, audio_file = download_video_and_audio("English in a Minute: Grow Lik
 text = generate_subtitles(audio_file)
 print("Converted Text:")
 print(text)
+
+with open('subtitles.txt', 'w') as file:
+    # Write the content of 'text' to the file
+    file.write(text)
